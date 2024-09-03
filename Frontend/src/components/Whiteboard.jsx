911417b9e535a43ec073e7c625c8b5b81389a86f
ahmyk9 <ahmedkhilji99@gmail.com>
2024-09-03 19:01:49 +1000

    created canvas

--- Frontend/src/components/Whiteboard.jsx
+++ Frontend/src/components/Whiteboard.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
-import Toolbox from './Toolbox'
+import React, { useEffect } from "react";
+import Toolbox from "./Toolbox";
+import { fabric } from 'fabric';
 
-const Whiteboard = () => {
+const Whiteboard = ({ canvasRef, toggleErase, changePenWidth, penWidth, changePenColor, penColor, setFabricCanvas, fabricCanvas, clearCanvas }) => {
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      backgroundColor: '#e5e7eb',
+      width: 1920,
+      height: 1080,
+      isDrawingMode: true,
+    });
+
+    setFabricCanvas(canvas);
+
+    return () => {
+      canvas.dispose();
+    };
+  }, [canvasRef, setFabricCanvas]);
+
+  useEffect(() => {
+    if (fabricCanvas) {
+      fabricCanvas.freeDrawingBrush.width = penWidth;
+      fabricCanvas.freeDrawingBrush.color = penColor;
+    }
+  }, [penWidth, penColor, fabricCanvas]);
+
   return (
-    <div className=''>
-
-        <div className='bg-gray-200 rounded-md w-full h-screen'>
-            <div className='p-4'>
-                <Toolbox ></Toolbox>
-            </div>
-        </div>
+    <div className="w-full h-screen bg-gray-200 rounded-md">
+      <div className="p-4">
+        <Toolbox
+          changePenWidth={changePenWidth}
+          penWidth={penWidth}
+          changePenColor={changePenColor}
+          penColor={penColor}
+          toggleErase={toggleErase} 
+          clearCanvas={clearCanvas}
+        />
+      </div>
+      <canvas ref={canvasRef} className=""></canvas>
     </div>
-  )
-}
+  );
+};
 
-export default Whiteboard
+export default Whiteboard;
