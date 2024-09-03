8a657bce30f4c982d54cfa5b625549e7386e2f78
ahmyk9 <ahmedkhilji99@gmail.com>
2024-09-03 19:01:49 +1000

    created canvas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 import Navbar from './components/Navbar';
-import Toolbox from './components/Toolbox';
 import Whiteboard from './components/Whiteboard';
+import { fabric } from 'fabric';
 
-function App() {
+const App = () => {
+  const canvasRef = useRef(null);
+  const [fabricCanvas, setFabricCanvas] = useState(null);
+  const defaultBackgroundColor = "#e5e7eb"
+
+  //toolbox states
+  const [penWidth, setPenWidth] = useState(1);
+  const [penColor, setPenColor] = useState("black");
+  const [toggleEraser , setToggleEraser] = useState(false)
+
+
+  const changePenWidth = (width) => {
+    if (fabricCanvas) {
+      const parsedWidth = parseInt(width, 10);
+      fabricCanvas.freeDrawingBrush.width = parsedWidth;
+      setPenWidth(parsedWidth);
+      fabricCanvas.renderAll();
+    }
+  };
+
+  const changePenColor = (color) => {
+    if (fabricCanvas) {
+      fabricCanvas.freeDrawingBrush.color = color;
+      setPenColor(color);
+      fabricCanvas.renderAll();
+    }
+  };
+
+  const downloadBoard = () => {
+    if (fabricCanvas) {
+      const pngData = fabricCanvas.toDataURL("png");
+      const downloadLink = document.createElement("a");
+      const fileName = `whiteBoard-session-${Math.random().toString().replace(".", "")}.png`;
+
+      downloadLink.href = pngData;
+      downloadLink.download = fileName;
+      downloadLink.click();
+    }
+  };
+
+  const toggleErase = () =>{
+    if (fabricCanvas) {
+      if(toggleEraser){
+        changePenColor("black")
+        setToggleEraser(false)
+      }
+      else{
+        changePenColor(defaultBackgroundColor)
+        setToggleEraser(true)
+      }
+
+    }
+  }
+
+
+  const clearCanvas = () => {
+    if (fabricCanvas) {
+      fabricCanvas.clear();
+      fabricCanvas.backgroundColor = defaultBackgroundColor; 
+      fabricCanvas.renderAll();
+    }
+  };
+
   return (
     <div>
-      <Navbar></Navbar>
-      <Whiteboard></Whiteboard>
+      <Navbar downloadBoard={downloadBoard} />
+      <Whiteboard
+        // for canvas
+        canvasRef={canvasRef}
+        setFabricCanvas={setFabricCanvas}
+        fabricCanvas={fabricCanvas}  
+        
+        // for toolbox passing through whiteboard
+        changePenWidth={changePenWidth}
+        penWidth={penWidth}
+
+        changePenColor={changePenColor}
+        penColor={penColor}
+        
+        setToggleEraser={setToggleEraser}
+        toggleErase={toggleErase}
+
+        clearCanvas={clearCanvas} 
+      />
     </div>
   );
-}
+};
 
 export default App;
--- Frontend/src/components/Toolbox.jsx
+++ Frontend/src/components/Toolbox.jsx
@@ -1,38 +1,62 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   PaintBrushIcon,
   PencilIcon,
   ServerIcon,
+  TrashIcon,
 } from "@heroicons/react/24/outline";
 
-export function Icons({ IconComponent }) {
+const Toolbox = ({ changePenWidth, penWidth, changePenColor, penColor, toggleErase, clearCanvas }) => {
   return (
-    <div className="h-10 w-10 hover:bg-gray-100 rounded-md p-2 cursor-pointer">
-      <IconComponent className="w-6 h-6" />
-    </div>
-  );
-}
+    <div>
+      <div className="flex items-center justify-between bg-white p-2 space-x-4 rounded-md w-[400px] shadow-lg">
+        {/* icons */}
+        <Icons IconComponent={PaintBrushIcon} />
+        <Icons IconComponent={PencilIcon} />
 
-const Toolbox = () => {
-  return (
-    <div className="flex bg-white p-2 items-center justify-between space-x-4 rounded-md w-[300px] shadow-lg">
-     
-    {/* icons  */}
-      <Icons IconComponent={PaintBrushIcon} />
-      <Icons IconComponent={PencilIcon} />
-      <Icons IconComponent={ServerIcon} />
+        <div onClick={toggleErase} className="cursor-pointer">
+          <Icons IconComponent={ServerIcon} />
+        </div>
 
-      <div className="w-full">
-  <input
-    type="range"
-    min="0"
-    max="100"
-    className="w-[100px] appearance-none h-2 bg-gray-200 rounded-lg cursor-pointer"
-  />
-</div>
+        {/* Color Chooser */}
+        <div className="p-2 cursor-pointer flex justify-center items-center hover:bg-gray-100 rounded-md h-10 w-10 relative">
+          <div className="w-5 h-5 border-2 border-black rounded relative">
+            <input
+              type="color"
+              onChange={(event) => changePenColor(event.target.value)}
+              value={penColor}
+              className="absolute inset-0 opacity-0 cursor-pointer"
+            />
+          </div>
+        </div>
 
+        {/* Pen Width */}
+        <div className="w-full flex">
+          <input
+            type="range"
+            onChange={(event) => changePenWidth(event.target.value)}
+            value={penWidth}
+            min="1"
+            max="30"
+            className="w-[100px] appearance-none h-2 bg-gray-200 rounded-lg cursor-pointer"
+          />
+        </div>
+
+        {/* Clear the Canvas */}
+        <div onClick={clearCanvas} className="cursor-pointer">
+          <Icons IconComponent={TrashIcon} />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Toolbox;
+
+const Icons = ({ IconComponent }) => {
+  return (
+    <div className="h-10 w-10 hover:bg-gray-100 rounded-md p-2">
+      <IconComponent className="w-6 h-6" />
+    </div>
+  );
+};
