2d66415303e83020218fa7ef21a359464b4f23e7
ahmyk9 <ahmedkhilji99@gmail.com>
2024-09-17 17:28:49 +1000

    add ai button in navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import { ArrowDownTrayIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import {ArrowDownTrayIcon} from "@heroicons/react/24/solid";
+import {SparklesIcon} from "@heroicons/react/24/outline";
 
-const Navbar = ({ downloadBoard }) => {
+const Navbar = ({downloadBoard}) => {
   return (
     <div>
-      <nav className='p-3 border-solid border-gray-600'>
-        <div className='flex items-center justify-between'>
-
+      <nav className="p-3 border-solid border-gray-600">
+        <div className="flex items-center justify-between">
           <div>
-            <h1 className='text-xl font-bold'>
-              Whiteboard Canvas
-            </h1>
+            <h1 className="text-xl font-bold">Whiteboard Canvas</h1>
           </div>
 
-          <button 
-            className='flex space-x-3'
-            type='button'
-            onClick={downloadBoard}
-          >
-            <IconWrapper IconComponent={ArrowDownTrayIcon} />
-          </button>
-
+          <div className="flex justify-center items-center">
+            <button
+              className="flex space-x-3 px-2"
+              type="button"
+              onClick={downloadBoard}>
+              <IconWrapper IconComponent={ArrowDownTrayIcon} />
+            </button>
+            <button
+              className="flex space-x-3 px-2"
+              type="button"
+              onClick={downloadBoard}>
+              <IconWrapper IconComponent={SparklesIcon} />
+            </button>
+          </div>
         </div>
       </nav>
     </div>
@@ -29,10 +33,10 @@ const Navbar = ({ downloadBoard }) => {
 
 export default Navbar;
 
-const IconWrapper = ({ IconComponent }) => {
+const IconWrapper = ({IconComponent}) => {
   return (
-    <div className='h-10 w-10 hover:bg-gray-100 rounded-md p-2 '>
-      <IconComponent className='w-6 h-6' />
+    <div className="h-10 w-10 hover:bg-gray-100 rounded-md p-2 ">
+      <IconComponent className="w-6 h-6" />
     </div>
   );
 };
