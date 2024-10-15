5fb60b9ff5a369cb3d68a437dc8dd722cca95e4d
Daniel Jacob <danieljacob9123@gmail.com>
2024-10-15 18:02:25 +1100

    Added button for AI selection

--- Frontend/src/components/Toolbox.jsx
+++ Frontend/src/components/Toolbox.jsx
@@ -15,6 +15,7 @@ import {
   CursorArrowRaysIcon as CursorArrowRaysIconSolid,
   BeakerIcon as BeakerIconSolid,
   ArrowDownTrayIcon,
+  CpuChipIcon,
 } from "@heroicons/react/24/solid";
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
@@ -32,13 +33,12 @@ const Toolbox = ({
   undo, 
   redo,
   downloadBoard,
-  handleAIClick
+  handleAIClick,
+  handleAISelection
 }) => {
-
-
   return (
     <div>
-      <div className="flex items-center justify-between bg-white p-2 space-x-4 rounded-md w-[750px] shadow-lg">
+      <div className="flex items-center justify-between bg-white p-2 space-x-4 rounded-md w-full max-w-[800px] shadow-lg">
         {/* Switch to regular cursor/move function */}
         <div onClick={() => setTool("cursor")} className="cursor-pointer">
           <Icons
@@ -72,51 +72,51 @@ const Toolbox = ({
             </MenuButton>
           </div>
           <MenuItems
-        transition
-        className="absolute z-10 mt-3 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <div className="py-1">
-          {/* Add Text Element to Canvas */}
-          <MenuItem>
-            <div 
-              onClick={() => addText()}
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
-            >
-              Text
-            </div> 
-          </MenuItem>
-          
-          {/* Add Square to Canvas */}
-          <MenuItem>
-            <div
-              onClick={() => addShape("square")}
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
-            >
-              □ Square
+            transition
+            className="absolute z-10 mt-3 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              {/* Add Text Element to Canvas */}
+              <MenuItem>
+                <div 
+                  onClick={() => addText()}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Text
+                </div> 
+              </MenuItem>
+              
+              {/* Add Square to Canvas */}
+              <MenuItem>
+                <div
+                  onClick={() => addShape("square")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  □ Square
+                </div>
+              </MenuItem>
+              
+              {/* Add Circle to Canvas */}
+              <MenuItem>
+                <div
+                  onClick={() => addShape("circle")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  ○ Circle
+                </div>
+              </MenuItem>
+              
+              {/* Add Triangle to Canvas */}
+              <MenuItem>
+                <div
+                  onClick={() => addShape("triangle")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  △ Triangle
+                </div>
+              </MenuItem>
             </div>
-          </MenuItem>
-          
-          {/* Add Circle to Canvas */}
-          <MenuItem>
-            <div
-              onClick={() => addShape("circle")}
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
-            >
-              ○ Circle
-            </div>
-          </MenuItem>
-          
-          {/* Add Triangle to Canvas */}
-          <MenuItem>
-            <div
-              onClick={() => addShape("triangle")}
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
-            >
-              △ Triangle
-            </div>
-          </MenuItem>
-        </div>
-      </MenuItems>
+          </MenuItems>
         </Menu>
 
         {/* Color Chooser */}
@@ -171,11 +171,55 @@ const Toolbox = ({
           <Icons IconComponent={TrashIcon} />
         </div>
 
+        {/* New Dropdown Button */}
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <MenuButton className="mt-1">
+              <Icons IconComponent={CpuChipIcon} />
+            </MenuButton>
+          </div>
+          <MenuItems
+            transition
+            className="absolute z-10 mt-3 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              {/* Option 1 */}
+              <MenuItem>
+                <div 
+                  onClick={() => handleAISelection("Option 1 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 1
+                </div> 
+              </MenuItem>
+              
+              {/* Option 2 */}
+              <MenuItem>
+                <div
+                  onClick={() => handleAISelection("Option 2 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 2
+                </div>
+              </MenuItem>
+              
+              {/* Option 3 */}
+              <MenuItem>
+                <div
+                  onClick={() => handleAISelection("Option 3 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 3
+                </div>
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </Menu>
+
         {/* AI Button */}
         <div onClick={handleAIClick} className="cursor-pointer">
           <Icons IconComponent={LightBulbIcon} />
         </div>
-
       </div>
     </div>
   );
