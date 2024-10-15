3a27a6b725d0d2f71114d974945aba5e51541e72
Daniel Jacob <danieljacob9123@gmail.com>
2024-10-15 18:02:25 +1100

    Added button for AI selection

--- Frontend/src/components/Toolbox.jsx
+++ Frontend/src/components/Toolbox.jsx
@@ -187,6 +187,56 @@ const Toolbox = ({
         <div onClick={() => setClearModal(true)} className="cursor-pointer">
           <Icons IconComponent={TrashIcon} />
         </div>
+
+        {/* New Dropdown Button */}
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <MenuButton className="mt-1">
+              <Icons IconComponent={CpuChipIcon} />
+            </MenuButton>
+          </div>
+          <MenuItems
+            transition
+            className="absolute z-10 mt-3 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              {/* Option 1 */}
+              <MenuItem>
+                <div 
+                  onClick={() => handleAISelection("Option 1 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 1
+                </div> 
+              </MenuItem>
+              
+              {/* Option 2 */}
+              <MenuItem>
+                <div
+                  onClick={() => handleAISelection("Option 2 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 2
+                </div>
+              </MenuItem>
+              
+              {/* Option 3 */}
+              <MenuItem>
+                <div
+                  onClick={() => handleAISelection("Option 3 instruction")}
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 cursor-pointer"
+                >
+                  Option 3
+                </div>
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </Menu>
+
+        {/* AI Button */}
+        <div onClick={handleAIClick} className="cursor-pointer">
+          <Icons IconComponent={LightBulbIcon} />
+        </div>
       </div>
     </div>
   );
