caa092da4ac90b4f806125ce082e4dd7c965541c
Jacob Moses <49137059+jacobmoses10@users.noreply.github.com>
2024-10-24 12:14:03 +1100

    Fixed crashing on zoom

--- Frontend/src/pages/Whiteboard.jsx
+++ Frontend/src/pages/Whiteboard.jsx
@@ -37,6 +37,7 @@ const Whiteboard = ({
 }) => {
   const { whiteboardId } = useParams();
   const desktopMode = useMediaQuery({ query: '(min-width: 1224px)' });
+  console.log("Desktop Mode: ", desktopMode);
   // Initialize the fabric canvas
   useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current, {
@@ -154,7 +155,11 @@ const Whiteboard = ({
       tabIndex={0}
     >
       {desktopMode ? (
-        <canvas ref={canvasRef} className="fixed shadow-lg my-4" />
+        <TransformWrapper disabled="true" limitToBounds="false">
+        <TransformComponent>
+          <canvas ref={canvasRef} className="fixed shadow-lg my-4" />          
+        </TransformComponent>
+      </TransformWrapper>
       ) : (
         <TransformWrapper disabled={transformDisabled} limitToBounds="false">
           <TransformComponent>
@@ -190,3 +195,17 @@ const Whiteboard = ({
 };
 
 export default Whiteboard;
+
+/*
+
+{desktopMode ? (
+        <canvas ref={canvasRef} className="fixed shadow-lg my-4" />
+      ) : (
+        <TransformWrapper disabled={transformDisabled} limitToBounds="false">
+          <TransformComponent>
+            <canvas ref={canvasRef} className="fixed shadow-lg my-4" />          
+          </TransformComponent>
+        </TransformWrapper>
+      )}
+
+*/
